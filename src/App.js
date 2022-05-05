3355c3fa0063f4ddc8da31a66e1f9e38b83dc762
rnzid <ranjitdangol13@gmail.com>
2022-05-05 23:17:43 +0545

    button work done

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './style.css'
+
+const App = ()=>{
+    return <div>
+        <h1>0</h1>
+        <button className="button">Add</button>
+        <button className="button">Less</button>
+        <button className="button">Reset</button>
+    </div>
+}
+
+export default App;
